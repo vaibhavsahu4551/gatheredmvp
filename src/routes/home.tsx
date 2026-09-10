0114899d12e7b23c311aa7468d7b2bdb0667228b
vaibhavsahu4551 <vaibhavsahu4551@gmail.com>
2026-09-10 23:18:36 +0530

    Delete src/routes/home.tsx

--- src/routes/home.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { HomeFeed } from "./_authenticated/_app/home";
-
-export const Route = createFileRoute("/home")({
-  component: HomeFeed,
-});
