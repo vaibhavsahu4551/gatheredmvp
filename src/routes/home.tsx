521dc21306307ac5721ee573ac014faecd189e11
vaibhavsahu4551 <vaibhavsahu4551@gmail.com>
2026-09-10 23:14:13 +0530

    Update home.tsx

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,1 +1,6 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { HomeFeed } from "./_authenticated/_app/home";
 
+export const Route = createFileRoute("/home")({
+  component: HomeFeed,
+});
